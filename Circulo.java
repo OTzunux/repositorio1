203793525f001217a85dfb1cd4fd0c55620bd8bb
Melvin <orlandotzunux@gmail.com>
2024-08-10 00:04:03 -0600

    ejemplo cargado

---
+++ Circulo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Circulo {
+    // Programa que Calcula el Area de un circulo por medio del radio
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        // Definimos las entradas y as constantes
+        double radio;
+        double pi = 3.1416;
+        System.out.print("Ingrese el radio del Circulo: ");
+        radio = entrada.nextInt();
+        // Carculo del radio
+        double area = pi*radio*radio;
+        // Imprimimos el resultado
+        System.out.println("El area del circulo es: "+area+" unidades");        
+    }
+    
+}
